11b33d66348d1ac10cbd44e7965d558a64404d50
Alan Bunjevac <alan.bunjevac@exobyte.org>
2018-09-02 14:53:31 +0200

    DiscoverArgumentValue tests

--- config_test.go
+++ config_test.go
@@ -43,3 +43,62 @@ func TestSplitArgs(t *testing.T) {
 		})
 	}
 }
+
+func Test_discoverArgumentValue(t *testing.T) {
+	type args struct {
+		args         []string
+		longName     string
+		shortName    string
+		defaultValue string
+	}
+
+	tests := []struct {
+		name    string
+		args    args
+		want    string
+		wantErr bool
+	}{
+		{
+			name: "short",
+			args: args{
+				args:         []string{"-k -c short.ini"},
+				longName:     "",
+				shortName:    "c",
+				defaultValue: "default.ini"},
+			want:    "short.ini",
+			wantErr: false},
+		{
+			name: "long",
+			args: args{
+				args:         []string{"-k --config config.ini"},
+				longName:     "config",
+				shortName:    "",
+				defaultValue: "default.ini"},
+			want:    "config.ini",
+			wantErr: false},
+		{
+			name: "default",
+			args: args{
+				args:         []string{"-k --noop"},
+				longName:     "config",
+				shortName:    "c",
+				defaultValue: "default.ini"},
+			want:    "default.ini",
+			wantErr: false},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := discoverArgumentValue(tt.args.args, tt.args.longName, tt.args.shortName, tt.args.defaultValue)
+
+			if (err != nil) != tt.wantErr {
+				t.Errorf("discoverArgumentValue() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+
+			if got != tt.want {
+				t.Errorf("discoverArgumentValue() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
